c5d1e785b4983f25c80b0bc9245ee55e05307b26
numbero <numbero@qq.com>
2025-01-05 23:24:35 +0800

    feat(ui): add interceptor feature

---
+++ src/main/java/org/example/singularpoint/ui/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package org.example.singularpoint.ui.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+    }
+}
